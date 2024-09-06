1d3f98ce0c163d5b2005772cd62ff46c8f3e5623
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-09-07 02:12:44 +0800

    add instagram section to home page

--- src/components/home/coverImage.tsx
+++ src/components/home/coverImage.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import config from "@/app/config.json";
+
+const { home } = config.pages;
+const { coverImage } = home.components;
+
 export const CoverImage = () => {
   return (
-    <div className="flex w-[100dvw] h-[100dvh] justify-center items-center bg-beige text-black">
-      Placeholder
+    <div className="flex w-full h-full max-h-[100dvh] justify-center items-center bg-beige text-black overflow-clip">
+      <Image
+        alt={coverImage.alt}
+        src={coverImage.src}
+        width={2000}
+        height={2000}
+      />
     </div>
   );
 };
--- src/components/home/instagram.tsx
+++ src/components/home/instagram.tsx
@@ -1,7 +1,50 @@
+import Image from "next/image";
+import { Logo } from "../logo";
+import config from "@/app/config.json";
+
+const { home } = config.pages;
+const { instagram } = home.components;
+
+const VisitPageButton = () => {
+  return (
+    <a href={instagram.visitPageButton.href}>
+      <button className="px-4 py-2 font-bold border-[1px] border-dark-brown">
+        {instagram.visitPageButton.text}
+      </button>
+    </a>
+  );
+};
+
+const LeftPanel = () => {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center p-12">
+      <Logo />
+      <p className="text-center text-xl">{instagram.content[0]}</p>
+      <p className="text-center text-xl">{instagram.content[1]}</p>
+      <p className="text-center font-bold text-xl">{instagram.content[2]}</p>
+      <VisitPageButton />
+    </div>
+  );
+};
+
+const RightPanel = () => {
+  return (
+    <div className="flex w-full h-full">
+      <Image
+        src={instagram.coverImage.src}
+        alt={instagram.coverImage.alt}
+        width={1000}
+        height={1000}
+      />
+    </div>
+  );
+};
+
 export const Instagram = () => {
   return (
-    <div className="flex w-[100dvw] h-[100dvh] justify-center items-center bg-dull-gray text-dark-brown">
-      Placeholder
+    <div className="grid grid-cols-2 w-[100dvw] h-fit bg-dull-gray text-dark-brown">
+      <LeftPanel />
+      <RightPanel />
     </div>
   );
 };
