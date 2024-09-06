edeeed26dc221ccfccab1d4bf182ea315fc3a4be
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-09-06 15:59:41 +0800

    add bg and text colours to home page components

--- src/components/home/coverImage.tsx
+++ src/components/home/coverImage.tsx
@@ -1,6 +1,6 @@
 export const CoverImage = () => {
   return (
-    <div className="flex w-[100dvw] h-[100dvh] justify-center items-center bg-beige">
+    <div className="flex w-[100dvw] h-[100dvh] justify-center items-center bg-beige text-black">
       Placeholder
     </div>
   );
