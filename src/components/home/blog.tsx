027da5361f78ea7e9f010fef5a3390a1d0fdb1c7
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-09-07 02:22:50 +0800

    add blog home page component

--- src/components/home/blog.tsx
+++ src/components/home/blog.tsx
@@ -1,8 +1,50 @@
+import Image from "next/image";
+import { Logo } from "../logo";
+import config from "@/app/config.json";
+
+const { home } = config.pages;
+const { blog } = home.components;
+
+const ReadMoreButton = () => {
+  return (
+    <a href={blog.readMoreButton.href}>
+      <button className="px-4 py-2 font-bold border-[1px] border-dark-brown">
+        {blog.readMoreButton.text}
+      </button>
+    </a>
+  );
+};
+
+const LeftPanel = () => {
+  return (
+    <div className="flex w-full h-full">
+      <Image
+        src={blog.coverImage.src}
+        alt={blog.coverImage.alt}
+        width={1000}
+        height={1000}
+      />
+    </div>
+  );
+};
+
+const RightPanel = () => {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center p-12">
+      <Logo />
+      <p className="text-center text-xl">{blog.content[0]}</p>
+      <p className="text-center text-xl">{blog.content[1]}</p>
+      <p className="text-center font-bold text-xl">{blog.content[2]}</p>
+      <ReadMoreButton />
+    </div>
+  );
+};
+
 export const Blog = () => {
-    return (
-      <div className="flex w-[100dvw] h-[100dvh] justify-center items-center bg-olive text-dull-gray">
-        Placeholder
-      </div>
-    );
-  };
-  
+  return (
+    <div className="grid grid-cols-2 w-[100dvw] h-fit bg-olive text-dull-gray">
+      <LeftPanel />
+      <RightPanel />
+    </div>
+  );
+};
