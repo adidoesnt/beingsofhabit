abecec201253d8c2c70d58053809012bdd03ef3c
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-09-07 11:06:26 +0800

    add activer wear and accessories component to home page

--- src/components/home/activeWearAndAccessories.tsx
+++ src/components/home/activeWearAndAccessories.tsx
@@ -1,7 +1,46 @@
+import config from "@/app/config.json";
+import Image from "next/image";
+
+const { home } = config.pages;
+const { activeWearAndAccessories } = home.components;
+
+const ShopNowButton = () => {
+  return (
+    <a href={activeWearAndAccessories.shopNowButton.href}>
+      <button className="px-4 py-2 border-[1px] border-dull-gray">
+        {activeWearAndAccessories.shopNowButton.text}
+      </button>
+    </a>
+  );
+};
+
+const LeftPanel = () => {
+  return (
+    <div className="flex flex-col p-16 gap-8">
+      <p className="text-left text-4xl">{activeWearAndAccessories.caption}</p>
+      <ShopNowButton />
+    </div>
+  );
+};
+
+const RightPanel = () => {
+  return (
+    <div className="flex w-full h-full">
+      <Image
+        src={activeWearAndAccessories.image.src}
+        alt={activeWearAndAccessories.image.alt}
+        width={1000}
+        height={1000}
+      />
+    </div>
+  );
+};
+
 export const ActiveWearAndAccessories = () => {
   return (
-    <div className="flex w-[100dvw] h-[100dvh] justify-center items-center bg-olive text-dull-gray">
-      Placeholder
+    <div className="grid grid-cols-2 w-[100dvw] h-fit bg-olive text-dull-gray">
+      <LeftPanel />
+      <RightPanel />
     </div>
   );
 };
