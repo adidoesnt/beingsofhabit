de87fa5a837fb747b9b6aa4d466b65e7906fc679
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-09-07 18:15:50 +0800

    add journals and books component to home page

--- src/components/home/journalsAndBooks.tsx
+++ src/components/home/journalsAndBooks.tsx
@@ -1,7 +1,58 @@
+import config from "@/app/config.json";
+import Image from "next/image";
+
+const { home } = config.pages;
+const { journalsAndBooks } = home.components;
+
+const ShopNowButton = () => {
+  return (
+    <a href={journalsAndBooks.shopNowButton.href}>
+      <button className="px-4 py-2 border-[1px] border-dull-gray">
+        {journalsAndBooks.shopNowButton.text}
+      </button>
+    </a>
+  );
+};
+
+const Content = () => {
+  return (
+    <div className="flex flex-col gap-2 items-start justify-center">
+      {journalsAndBooks.content.map((content) => (
+        <p key={content} className="text-center text-4xl">
+          {content}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+const LeftPanel = () => {
+  return (
+    <div className="flex flex-col justify-start items-start p-12 gap-12">
+      <Content />
+      <ShopNowButton />
+    </div>
+  );
+};
+
+const RightPanel = () => {
+  return (
+    <div className="flex w-full h-full">
+      <Image
+        src={journalsAndBooks.image.src}
+        alt={journalsAndBooks.image.alt}
+        width={1000}
+        height={1000}
+      />
+    </div>
+  );
+};
+
 export const JournalsAndBooks = () => {
   return (
-    <div className="flex w-[100dvw] h-[100dvh] justify-center items-center bg-olive text-dull-gray">
-      Placeholder
+    <div className="grid grid-cols-2 justify-center items-center bg-olive text-dull-gray">
+      <LeftPanel />
+      <RightPanel />
     </div>
   );
 };
