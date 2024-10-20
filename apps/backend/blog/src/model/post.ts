46c60edb45955dc26eb8257b1873deca89ad461d
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-10-20 16:04:52 +0800

    add post model

---
+++ apps/backend/blog/src/model/post.ts
@@ -0,0 +1,39 @@
+import { getModelForClass, prop } from "@typegoose/typegoose";
+
+enum Category {
+  FITNESS = "fitness",
+  NUTRITION = "nutrition",
+  WELLBEING = "wellbeing & self-care",
+  LEARNING = "learning",
+}
+
+class Post {
+  @prop({ required: true, type: String })
+  declare title: string;
+
+  @prop({ required: true, type: String })
+  declare blurb: string;
+
+  @prop({ required: true, type: String })
+  declare content: string;
+
+  @prop({ required: true, type: String })
+  declare category: Category;
+
+  @prop({ required: true, type: String })
+  declare headerImageURL: string;
+
+  @prop({ required: true, type: String })
+  declare author: string;
+
+  @prop({ required: true, type: Date })
+  declare releaseDate: Date;
+}
+
+export const PostModel = getModelForClass(Post, {
+  schemaOptions: {
+    collection: "posts",
+    timestamps: true,
+    _id: true,
+  },
+});
