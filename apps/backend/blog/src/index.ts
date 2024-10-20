698b254f2e5f948aef3836e6d5ea0f86e74eddc0
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-10-20 15:01:39 +0800

    add boilerplate for blog service

--- apps/backend/blog/src/index.ts
+++ apps/backend/blog/src/index.ts
@@ -1,7 +1,18 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+try {
+  const { PORT = 3004, NODE_ENV = "PROD" } = process.env;
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  const app = new Elysia().get("/", () => "Hello Elysia").listen(PORT);
+  const { hostname, port } = app.server ?? {};
+
+  if (!hostname || !port) {
+    throw new Error("💀 Failed to start Elysia server");
+  }
+
+  const prefix = NODE_ENV === "DEV" ? "http://" : "https://";
+  console.log(`🦊 Blog service is running at ${prefix}${hostname}:${port}`);
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
