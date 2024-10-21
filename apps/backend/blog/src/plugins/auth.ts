e5d69c5b020a4c01c8d3c47db12d06cb6e8ca4b8
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-10-21 14:30:38 +0800

    add logic to auth plugin implementation

--- apps/backend/blog/src/plugins/auth.ts
+++ apps/backend/blog/src/plugins/auth.ts
@@ -1,7 +1,20 @@
 import { Elysia } from "elysia";
+import { verifyToken } from "../utils/jwt";
+import { Status } from "../constants";
 
 export const authPlugin = () => {
-    console.log("Setting up auth plugin");
+  console.log("Setting up auth plugin");
 
-    return new Elysia();
+  return new Elysia().onBeforeHandle(({ request, set }) => {
+    const token = request.headers.get("Authorization");
+    if (!token) return;
+
+    try {
+      const isValid = verifyToken(token);
+      if (!isValid) throw new Error("Invalid token");
+    } catch (error) {
+      set.status = Status.UNAUTHORIZED;
+      return;
+    }
+  });
 };
