fd374b50bcbf29c4dc84e64bd923f50c64380cad
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-10-22 13:16:36 +0800

    add react query

--- apps/frontend/blog-portal/src/routes/__root.tsx
+++ apps/frontend/blog-portal/src/routes/__root.tsx
@@ -1,18 +1,24 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 const { VITE_NODE_ENV: NODE_ENV = "PROD" } = import.meta.env;
 const isDevEnv = NODE_ENV === "DEV";
 
+const queryClient = new QueryClient();
+
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Outlet />
-      {isDevEnv &&<TanStackRouterDevtools position="bottom-right" />}
-    </>
-  )
+      {isDevEnv && <TanStackRouterDevtools position="bottom-right" />}
+    </QueryClientProvider>
+  );
 }
