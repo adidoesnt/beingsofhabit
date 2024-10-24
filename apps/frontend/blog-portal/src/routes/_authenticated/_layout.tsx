14a25b552301cb187c4c2acf162293a6221093a8
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-10-24 19:04:56 +0800

    add profile avatar and hover menu

--- apps/frontend/blog-portal/src/routes/_authenticated/_layout.tsx
+++ apps/frontend/blog-portal/src/routes/_authenticated/_layout.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/AppSideBar";
+import { ProfileAvatar } from "@/components/ProfileAvatar";
 
 export const Route = createFileRoute("/_authenticated/_layout")({
   component: LayoutComponent,
@@ -12,6 +13,7 @@ function LayoutComponent() {
       <div className="flex gap-4 h-[100dvh] w-full items-center">
         <AppSidebar />
         <SidebarTrigger className="absolute top-0 left-0 z-10 m-4" />
+        <ProfileAvatar className="absolute top-0 right-0 z-10 m-4" />
         <Outlet />
       </div>
     </SidebarProvider>
--- packages/utils/lib/string.ts
+++ packages/utils/lib/string.ts
@@ -1,3 +1,8 @@
 export const toFirstLetterUppercase = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
+
+export const getInitials = (str: string) => {
+  if (str.length === 1) return str.charAt(0).toUpperCase();
+  return str.split("").slice(0, 2).join("").toUpperCase();
+};
