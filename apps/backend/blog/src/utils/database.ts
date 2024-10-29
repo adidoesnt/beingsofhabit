60ac9d9e4bc167b648b4d7a2df46263ea9dcfd5f
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-10-29 11:33:34 +0800

    update blog database utils

--- apps/backend/blog/src/utils/database.ts
+++ apps/backend/blog/src/utils/database.ts
@@ -1,14 +1,49 @@
-import { connect } from "mongoose";
+import { connect, ConnectOptions, mongo } from "mongoose";
 import { logger } from "./logger";
+import path from "path";
+import fs from "fs";
+
+const { MONGODB_URI = "DUMMY-URL", MONGODB_DB_NAME = "DUMMY-DB-NAME", NODE_ENV = "PROD" } =
+    process.env;
+const isDevEnv = NODE_ENV === "DEV";
+
+const getGlobalBundleFilePath = () => {
+  if(isDevEnv) return;
+
+  const globalBundleFilePath = path.resolve(__dirname, "../../global-bundle.pem");
+
+  logger.debug("🔑 Loading global bundle file...", {
+    globalBundleFilePath,
+  });
+
+  if (!fs.existsSync(globalBundleFilePath) && isDevEnv) {
+    throw new Error("Global bundle file not found");
+  }
+
+  return globalBundleFilePath;
+}
 
 const connectToDatabase = async () => {
-  const { MONGODB_URI = "DUMMY-URL", MONGODB_DB_NAME = "DUMMY-DB-NAME" } =
-    process.env;
+  const globalBundleFilePath = getGlobalBundleFilePath();
 
-  logger.info("Connecting to database...");
+  const options: ConnectOptions = {
+    dbName: MONGODB_DB_NAME
+  };
+
+  const optionsWithTls = { 
+    ...options, 
+    tls: true,
+    tlsCAFile: globalBundleFilePath
+  }
 
   try {
-    await connect(`${MONGODB_URI}/${MONGODB_DB_NAME}`);
+    if (isDevEnv) {
+      logger.debug("Connecting to database...", { options });
+      await connect(MONGODB_URI, options);
+    } else {
+      logger.debug("Connecting to database...", { options: optionsWithTls });
+      await connect(MONGODB_URI, optionsWithTls);
+    }
     logger.info("🌱 Connected to database");
   } catch (error) {
     logger.error("💀 Failed to connect to database:", error as Error);
