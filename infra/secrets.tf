c908bfd326f504e4976bd031ca059837a60f7cdf
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-11-10 15:07:39 +0800

    add doc db uri secret

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -24,3 +24,24 @@ resource "aws_secretsmanager_secret_version" "blog_docdb_credentials_version" {
     random_password.blog_docdb_password
   ]
 }
+
+resource "aws_secretsmanager_secret" "blog_docdb_uri" {
+  name = "blog/docdb_uri"
+
+  tags = {
+    Name = "blog-docdb-uri"
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "docdb_uri_version" {
+  secret_id = aws_secretsmanager_secret.blog_docdb_uri.id
+  secret_string = "mongodb://root:${random_password.blog_docdb_password.result}@${
+    aws_docdb_cluster.blog_docdb_cluster.endpoint
+  }:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+
+  depends_on = [
+    aws_secretsmanager_secret.blog_docdb_uri,
+    aws_docdb_cluster.blog_docdb_cluster,
+    random_password.blog_docdb_password
+  ]
+}
