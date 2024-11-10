b0cfe2d9b48431b6e1ac5a39c76388151d34c1bd
Aditya Banerjee <aditya_banerjee@icloud.com>
2024-11-10 14:33:42 +0800

    update vpc

--- infra/main.tf
+++ infra/main.tf
@@ -6,16 +6,6 @@ variable "aws_region" {
   type    = string
 }
 
-variable "blog_service_port" {
-  type    = number
-  default = 3004
-}
-
-variable "blog_docdb_username" {
-  type    = string
-  default = "root"
-}
-
 provider "aws" {
   profile = var.aws_profile
   region  = var.aws_region
--- infra/vpc.tf
+++ infra/vpc.tf
@@ -1,111 +1,70 @@
-variable "main_vpc" {
-    type = object({
-      cidr_block = string
-      name = string
-    })
-
-    default = {
-      cidr_block = "10.0.0.0/16"
-      name = "Beings of Habit VPC"
-    }
-}
-
-variable "igw_name" {
-  type    = string
-  default = "Beings of Habit Internet Gateway"
-}
-
-variable "public_subnet_a" {
-    type = object({
-      name = string
-      cidr_block = string
-      availability_zone = string
-    })
-
-    default = {
-      name = "Beings of Habit Public Subnet A"
-      cidr_block = "10.0.0.0/20"
-      availability_zone = "ap-southeast-1a"
-    }
-}
-
-variable "public_subnet_b" {
-    type = object({
-      name = string
-      cidr_block = string
-      availability_zone = string
-    })
-
-    default = {
-      name = "Beings of Habit Public Subnet B"
-      cidr_block = "10.0.16.0/20"
-      availability_zone = "ap-southeast-1b"
-    }
-}
-
-variable "route_table_name" {
-    type = string
-    default = "Beings of Habit Public Route Table"
-}
-
-resource "aws_vpc" "main" {
-  cidr_block = var.main_vpc.cidr_block
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = false
-  enable_dns_support = true
-  tags = {
-    Name = var.main_vpc.name
-  }
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
 
   tags = {
-    Name = var.igw_name
+    name = "Beings of Habit IGW"
   }
+
+  depends_on = [aws_vpc.main_vpc]
 }
 
-resource "aws_subnet" "public_a" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.public_subnet_a.cidr_block
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.0.0/20"
   map_public_ip_on_launch = false
-  availability_zone = var.public_subnet_a.availability_zone
+  availability_zone       = "ap-southeast-1a"
 
   tags = {
-    Name = var.public_subnet_a.name
+    name = "Beings of Habit Public Subnet A"
   }
+
+  depends_on = [aws_vpc.main_vpc]
 }
 
-resource "aws_subnet" "public_b" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.public_subnet_b.cidr_block
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.16.0/20"
   map_public_ip_on_launch = false
-  availability_zone = var.public_subnet_b.availability_zone
+  availability_zone       = "ap-southeast-1b"
 
   tags = {
-    Name = var.public_subnet_b.name
+    name = "Beings of Habit Public Subnet B"
   }
+
+  depends_on = [aws_vpc.main_vpc]
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "main_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+    gateway_id = aws_internet_gateway.main_igw.id
   }
 
   tags = {
-    Name = var.route_table_name
+    name = "Beings of Habit Public Route Table"
   }
+
+  depends_on = [aws_internet_gateway.main_igw]
 }
 
 resource "aws_route_table_association" "public_a" {
-  subnet_id = aws_subnet.public_a.id
-  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.main_route_table.id
+
+  depends_on = [aws_route_table.main_route_table, aws_subnet.public_subnet_a]
 }
 
 resource "aws_route_table_association" "public_b" {
-  subnet_id = aws_subnet.public_b.id
-  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.main_route_table.id
+
+  depends_on = [aws_route_table.main_route_table, aws_subnet.public_subnet_b]
 }
